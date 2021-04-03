d4a60c7c276ac0708af41a1e6647e8eb4437fb48
Kyrylo Polezhaiev <kirushyk@gmail.com>
2021-04-03 03:46:42 +0300

    model_factory: Lazy ref and cache list of possible concrete model variants

--- src/models/model_factory.rs
+++ src/models/model_factory.rs
@@ -43,41 +43,21 @@ lazy_static! {
         m.insert("Storage", storage_new as ModelConstructor);
         m
     };
+    static ref VARIANTS: Vec<&'static str> = {
+        CONSTRUCTORS.iter().map(|(k, _)| k).map(|&x| x).collect::<Vec<_>>()
+    };
 }
 
 pub fn create<'de, D: Deserializer<'de>>(model_type: &str, extra_fields: serde_yaml::Value) -> Result<Box<dyn AsModel>, D::Error> {
-    const VARIANTS: &'static [&'static str] = &[
-        &"Generator", &"ExclusiveGateway", &"Processor", &"Storage"
-    ];
     match CONSTRUCTORS.get(model_type) {
         Some(constructor) => {
             match constructor(extra_fields) {
                 Some(model) => Ok(model),
-                None => Err(de::Error::unknown_variant(model_type, VARIANTS))
+                None => Err(de::Error::unknown_variant(model_type, &VARIANTS))
             }
         },
         None => {
-            Err(de::Error::unknown_variant(model_type, VARIANTS))
+            Err(de::Error::unknown_variant(model_type, &VARIANTS))
         }
     }
-    // match model_type {
-    //     "Generator" => {
-    //         let generator = serde_yaml::from_value::<super::Generator>(extra_fields).map_err(de::Error::custom)?;
-    //         Ok(Box::new(generator))
-    //     },
-    //     "ExclusiveGateway" => {
-    //         let exclusive_gateway = serde_yaml::from_value::<super::ExclusiveGateway>(extra_fields).map_err(de::Error::custom)?;
-    //         Ok(Box::new(exclusive_gateway))
-    //     },
-    //     "Processor" => {
-    //         let processor = serde_yaml::from_value::<super::Processor>(extra_fields).map_err(de::Error::custom)?;
-    //         Ok(Box::new(processor))
-    //     },
-    //     "Storage" => {
-    //         let storage = serde_yaml::from_value::<super::Storage>(extra_fields).map_err(de::Error::custom)?;
-    //         Ok(Box::new(storage))
-    //     },
-    //     other => {
-    //     }
-    // }
 }
