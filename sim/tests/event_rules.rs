d8c3f8dedf7cfc2c244e1486926b727cc8194164
Neal DeBuhr <ndebuhr@gmail.com>
2022-02-21 21:09:02 +0000

    Fix a typo in the batcher_event_rules test error handling

--- sim/tests/event_rules.rs
+++ sim/tests/event_rules.rs
@@ -12,7 +12,7 @@ fn batcher_event_rules() {
     let batcher = Batcher::new(String::from("job"), String::from("job"), 0.5, 10, false);
 
     let batcher_event_rules = fs::read_to_string("tests/batcher_event_rules.json")
-        .expect("Unable to read tests/generator_event_rules.json");
+        .expect("Unable to read tests/batcher_event_rules.json");
 
     assert_eq!(batcher.event_rules(), batcher_event_rules);
 }
