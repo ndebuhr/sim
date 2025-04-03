ee29f8a1479d505c297d50d67dd11f6d63ec0b98
Bruce W. Lowther <bwlowther@micron.com>
2025-04-03 17:41:43 -0400

    Adding commenting and cleaning up tests.

--- sim/tests/evaluate_polynomial.rs
+++ sim/tests/evaluate_polynomial.rs
@@ -4,8 +4,7 @@ use sim::utils::evaluate_polynomial;
 fn test_evaluate_polynomial_base() {
     let coefficients = vec![1.0];
     let x: f64 = 1.0;
-    let y = evaluate_polynomial(&coefficients, x);
-    let actual_y = evaluate_polynomial(&coefficients, 1.0).ok().unwrap();
+    let actual_y = evaluate_polynomial(&coefficients, x).ok().unwrap();
     let expected_y = (1.0 * x.powf(0.0));
     assert_eq!(actual_y, expected_y);
 }
@@ -14,7 +13,6 @@ fn test_evaluate_polynomial_base() {
 fn test_evaluate_polynomial_two() {
     // coefficients ordered as specified in comments.
     let coefficients = vec![1.0, 0.3];
-    // let coefficients = vec![0.3, 1.0];
     let x: f64 = 1.0;
     let actual_y = evaluate_polynomial(&coefficients, x).ok().unwrap();
     let expected_y = (1.0 * x.powf(1.0)) + (0.3 * x.powf(0.0));
@@ -25,7 +23,6 @@ fn test_evaluate_polynomial_two() {
 fn test_evaluate_polynomial_sem() {
     // coefficients ordered as specified in comments.
     let coefficients = vec![2.0, -3.0, 1.0, -2.0, 3.0];
-    // let coefficients = vec![3.0, -2.0, 1.0, -3.0, 2.0];
     let x: f64 = 2.0;
     let actual_y = evaluate_polynomial(&coefficients, x).ok().unwrap();
     let expected_y = (2.0 * x.powf(4.0))
