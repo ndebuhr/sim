c43df28df66376c1d90ba64b5112271378c23c13
Bruce W. Lowther <brucelowther@gmail.com>
2025-05-13 17:05:56 -0400

    Organized storage_bench a little

--- sim/benches/storage_bench.rs
+++ sim/benches/storage_bench.rs
@@ -7,10 +7,23 @@ mod test_models {
     use sim::models::{DevsModel, Model, ModelMessage, Storage};
     use sim::simulator::Services;
     use test::Bencher;
+    use js_sys::Math::exp;
 
-    #[bench]
-    fn storage_bench(b: &mut Bencher) {
-        let mut model = Model::new(
+    fn put_message(content: String) -> ModelMessage{
+        ModelMessage {
+            port_name: "put".to_string(),
+            content: content,
+        }
+    }
+    fn get_message() -> ModelMessage {
+        ModelMessage {
+            port_name: "get".to_string(),
+            content: "NA01".to_string(),
+        }
+    }
+
+    fn get_bench_storage() -> Model {
+        Model::new(
             "bench_storage".to_string(),
             Box::new(Storage::new(
                 "put".to_string(),
@@ -18,56 +31,53 @@ mod test_models {
                 "stored".to_string(),
                 false,
             )),
-        );
-        let message = ModelMessage {
-            port_name: "store".to_string(),
-            content: "value001".to_string(),
-        };
-
-        b.iter(|| {
-            let mut services = Services::default();
-            let ext_result = model.events_int(&mut services);
-            let int_result = model.events_ext(&message, &mut services);
-        });
+        )
     }
 
     #[test]
+    ///verify that the storage model stores and retrieves a stored value.
     fn storage_test() {
-        let mut model = Model::new(
-            "bench_storage".to_string(),
-            Box::new(Storage::new(
-                "put".to_string(),
-                "get".to_string(),
-                "stored".to_string(),
-                false,
-            )),
-        );
-        let message = ModelMessage {
-            port_name: "put".to_string(),
-            content: "value001".to_string(),
-        };
-
+        let mut model = get_bench_storage();
+        let expected_message = "value001".to_string();
+        let put_message = put_message(expected_message.clone());
+        let get_message = get_message();
         let mut services = Services::default();
-        let ext_result = model.events_ext(&message, &mut services);
+        let ext_result = model.events_ext(&put_message, &mut services);
         assert!(ext_result.is_ok());
 
         let int_results = model.events_int(& mut services);
         assert!(int_results.is_ok());
-        
-        let message = ModelMessage {
-            port_name: "get".to_string(),
-            content: "ignored".to_string(),
-        };
-        let ext_result = model.events_ext(&message, &mut services);
+
+        let ext_result = model.events_ext(&get_message, &mut services);
         assert!(ext_result.is_ok());
 
         let int_results = model.events_int(& mut services);
-        assert!(int_results.is_ok());
-        
+        match int_results {
+            Ok(vl) => {
+                assert_eq!(vl.len(), 1);
+                assert_eq!(vl[0].content, expected_message.clone());
+            },
+            Err(_) => assert!(int_results.is_err())
+        }
     }
-}
 
-// warning: `sim` (bench "storage_bench") generated 2 warnings
-//     Finished `bench` profile [optimized] target(s) in 0.04s
-//      Running benches/storage_bench.rs (target/release/deps/storage_bench-016615949a18d51a)
-// 26.23022772606383 ns/iter (+/- 0.5808988530585104)
+
+    #[bench]
+    fn storage_bench(b: &mut Bencher) {
+        let mut model = get_bench_storage();
+        
+        let put_message = put_message("value001".to_string());
+        let get_message = get_message(); 
+
+        b.iter(|| {
+            let mut services = Services::default();
+            let ext_result = model.events_ext(&put_message, &mut services);
+            let int_result = model.events_int(&mut services);
+            let ext_result = model.events_ext(&get_message, &mut services);
+            let int_result = model.events_int(&mut services);
+            
+        });
+    }
+// test test_models::storage_bench ... bench:         165.55 ns/iter (+/- 2.20)
+// test test_models::storage_bench ... bench:         165.97 ns/iter (+/- 2.58)      
+}
